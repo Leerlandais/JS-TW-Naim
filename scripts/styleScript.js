6dd147b45c2c884b0122b9170e49180f254754d0
leerlandais <leerlandais@gmail.com>
2024-06-29 01:33:45 +0100

    prep for tomorrow

--- scripts/styleScript.js
+++ scripts/styleScript.js
@@ -2,19 +2,23 @@
 const   mainHeading = document.getElementById("mainHeading"),
         globalDiv   = document.getElementById("globalDiv");
 mainHeading.className   = "block font-sans text-5xl font-semibold leading-tight tracking-normal text-gray-700 underline italic antialiased";
-mainHeading.textContent = "Bienvenue à mon projet du weekend";
+mainHeading.textContent = " à mon projet du weekend";
 globalDiv.className     = "container-fluid flex justify-center pt-16 mx-auto bg-gradient-to-t from-green-400 to-sky-600 h-screen";
 
 let theUrl  = window.location.href,
     getLast = theUrl.split('/').pop();
 let ourPage = getLast.replace('.html', '');
 
+console.log('this ; ' + ourPage); // première con.log - je n'ai plus besoin de verifier chaque étape mais ceci est nouveau pour moi, alors je .log :D
 // maintenant, je sais sur quel page est affiché. La reste est facile
-console.log('this ; ' + ourPage);
 
-
-/*
 switch (ourPage) {
-    case 'index': etc etc etc etc etc etc etc
+    case 'index':
+        mainHeading.textContent = "You are home";
+        break;
+    case "exe-0-jokes" :
+        mainHeading.textContent = "exe-0-hahahah";
+        break;
+// put more cases here
+
 }
-*/
