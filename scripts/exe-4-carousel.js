c8933c2a4f9fc0dff9590a09e215fb4a1eb9417f
leerlandais <leerlandais@gmail.com>
2024-06-29 14:49:08 +0100

    moved css and added stop/start button

--- scripts/exe-4-carousel.js
+++ scripts/exe-4-carousel.js
@@ -1,5 +1,7 @@
-const windowHolder = document.querySelector(".windowHolder"),
-      portalWindow = document.querySelector('.portalWindow'),
+const windowHolder  = document.querySelector(".windowHolder"),
+      portalWindow  = document.querySelector('.portalWindow'),
+      countdown= document.getElementById('countdown'),
+      carouselControl= document.getElementById("carouselControl"),
       imageArray = ['url("images/MoustacheGlasses.svg"',
                             'url("images/PurpleDevil.svg"',
                             'url("images/StarsEyes.svg"',
@@ -8,20 +10,51 @@ const windowHolder = document.querySelector(".windowHolder"),
                             'url("images/TongueSmile.svg"',
                             'url("images/WinkSmile.svg"',
                             'url("images/TongueSquint.svg"' ];
-let count = 0;
+let count = 0,
+    carStop = true,
+    spinInterval;
     windowHolder.addEventListener("click", makeItSpin);
 portalWindow.style.backgroundImage = imageArray[0];
-
+carouselControl.addEventListener("click", stopStartCarousel)
 
 function makeItSpin() {
-    setInterval(() => {
+    let i = 2;
+    countdown.textContent = "3";
+
+    let countdownTimer =setInterval( () => {
+        if (i > 0){
+            countdown.textContent = i.toString();
+            i--;
+        }else {
+            countdown.textContent = "";
+            clearInterval(countdownTimer);
+        }
+    },1000);
+
+
+    spinInterval= setInterval(() => {
+
     count++;
 
     windowHolder.classList.toggle("spinIt");
     setTimeout(function () {
         portalWindow.style.backgroundImage = imageArray[count % imageArray.length]
-    }, 850);
+    }, 680);
     }, 3000);
+}
+
+function stopStartCarousel () {
+    console.log(carStop);
+    if (carStop) {
+        clearInterval(spinInterval);
+        carStop = false;
+        carouselControl.textContent = 'Activer';
+    } else {
+        makeItSpin();
+        carStop = true;
+        carouselControl.textContent = 'Désactiver';
+    }
+
 }
 
 makeItSpin();
