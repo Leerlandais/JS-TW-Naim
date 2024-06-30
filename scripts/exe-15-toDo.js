95c39561f1c99a2126c7c616cfbfbe14782f101e
leerlandais <leerlandais@gmail.com>
2024-06-30 11:39:24 +0100

    add and remove items : OK

--- scripts/exe-15-toDo.js
+++ scripts/exe-15-toDo.js
@@ -23,7 +23,14 @@ if (!toDoItem) {
 
 function createToDoList(ItemArray) {
     for (let item of ItemArray) {
-        console.log(item);
+       let addLI = document.createElement("li");
+       addLI.textContent = item;
+        let XX = document.createElement("button");
+        XX.classList.add("removeButton");
+        XX.textContent = "XX";
+        XX.addEventListener("click", removeListItem);
+        addLI.appendChild(XX);
+       toDoItemUL.appendChild(addLI);
     }
 }
 
@@ -34,4 +41,19 @@ function addNewToDoItem() {
     let addItem = JSON.stringify(toDoItemList);
     localStorage.setItem("toDoItems", addItem);
     window.location.reload();
+}
+
+function removeListItem() {
+    let itemToRemove = this.parentElement.textContent;
+    toDoItemList.splice(toDoItemList.indexOf(itemToRemove), 1);
+    console.log(toDoItemList.length);
+    if (toDoItemList.length === 0) {
+        localStorage.removeItem("toDoItems");
+    }else {
+    let deletedItemArray = JSON.stringify(toDoItemList);
+    localStorage.setItem("toDoItems", deletedItemArray);
+    this.parentElement.remove();
+    }
+    window.location.reload();
+
 }
