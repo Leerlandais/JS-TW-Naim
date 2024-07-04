4c51e1c50ec9a642ae2e1b5f0e1c93b0dc04245c
leerlandais <leerlandais@gmail.com>
2024-07-04 19:37:25 +0100

    btn options : OK - comments added

--- scripts/exe-16-snake.js
+++ scripts/exe-16-snake.js
@@ -1,3 +1,6 @@
+// pour ceci, je mis le commentaire dès que j'ai completer un étape. J'ai déjà fait un Snake donc ce n'est pas facile à révister ce logique
+// par contre, mon niveau de JS a evolué depuis Snake V1. Alors, comments, comments, comments
+
 const canvas = document.getElementById("snake");
 
 // afin d'avoir qqchose sur le canvas
@@ -9,16 +12,16 @@ if (canvas.getContext) {
 
 // première fois que je l'essai mais pourquoi pas écouter le DOM entière
 document.addEventListener('keydown', function(btnPressed) {
-    // ça marche?
-    console.log(btnPressed.code);
-    if (btnPressed.code === 'ArrowLeft') {
+    console.log(btnPressed.code); // mon mieux ami, console.log
+    if (btnPressed.code === 'ArrowLeft' || btnPressed.code === 'Numpad4' || btnPressed.code === 'KeyA') {
         console.log("left");
-    }else if (btnPressed.code === 'ArrowRight') {
+    }else if (btnPressed.code === "ArrowRight" || btnPressed.code === "Numpad6" || btnPressed.code === "KeyD") {
         console.log("R");
-    }else if (btnPressed.code === 'ArrowUp') {
+    }else if (btnPressed.code === 'ArrowUp' || btnPressed.code === 'Numpad8' || btnPressed.code === 'KeyW') {
         console.log("U");
-    }else if (btnPressed.code === 'ArrowDown') {
+    }else if (btnPressed.code === 'ArrowDown' || btnPressed.code === 'Numpad2' || btnPressed.code === 'KeyS') {
         console.log("Dn");
         // all good. Gotta cook soon but gonna add the alternative btn configs first
+        // that worked. Should I add a choose buttons option?
     }
 });
