de47933a7376271a20a8c341afcd27f7855bb1fa
leerlandais <leerlandais@gmail.com>
2024-07-04 20:06:29 +0100

    screen move : fixed

--- scripts/exe-16-snake.js
+++ scripts/exe-16-snake.js
@@ -1,10 +1,10 @@
 // pour ceci, je mis le commentaire dès que j'ai completer un étape. J'ai déjà fait un Snake donc ce n'est pas facile à révister ce logique
 // par contre, mon niveau de JS a evolué depuis Snake V1. Alors, comments, comments, comments
 
-const canvas = document.getElementById("snake");
-
+const canvas = document.getElementById("snake"),
+      context = canvas.getContext('2d');
 // afin d'avoir qqchose sur le canvas
-if (canvas.getContext) {
+if (context) {
     const ctx = canvas.getContext("2d");
     ctx.fillStyle = "rgb(0 0 200)";
     ctx.fillRect(10, 10, 20, 20);
@@ -13,15 +13,23 @@ if (canvas.getContext) {
 // première fois que je l'essai mais pourquoi pas écouter le DOM entière
 document.addEventListener('keydown', function(btnPressed) {
     console.log(btnPressed.code); // mon mieux ami, console.log
+    // Si Up/Down, ne bouge pas l'écran
+    if (btnPressed.code === 'ArrowUp' || btnPressed.code === 'ArrowDown' || btnPressed.code === 'Numpad8' || btnPressed.code === 'Numpad2') {
+        btnPressed.preventDefault();
+    }
     if (btnPressed.code === 'ArrowLeft' || btnPressed.code === 'Numpad4' || btnPressed.code === 'KeyA') {
         console.log("left");
-    }else if (btnPressed.code === "ArrowRight" || btnPressed.code === "Numpad6" || btnPressed.code === "KeyD") {
+        } else if (btnPressed.code === 'ArrowRight' || btnPressed.code === 'Numpad6' || btnPressed.code === 'KeyD') {
         console.log("R");
-    }else if (btnPressed.code === 'ArrowUp' || btnPressed.code === 'Numpad8' || btnPressed.code === 'KeyW') {
+     } else if (btnPressed.code === 'ArrowUp' || btnPressed.code === 'Numpad8' || btnPressed.code === 'KeyW') {
         console.log("U");
-    }else if (btnPressed.code === 'ArrowDown' || btnPressed.code === 'Numpad2' || btnPressed.code === 'KeyS') {
+        } else if (btnPressed.code === 'ArrowDown' || btnPressed.code === 'Numpad2' || btnPressed.code === 'KeyS') {
         console.log("Dn");
-        // all good. Gotta cook soon but gonna add the alternative btn configs first
-        // that worked. Should I add a choose buttons option?
     }
 });
+
+        // all good. Gotta cook soon but gonna add the alternative btn configs first
+        // that worked. Should I add a choose buttons option?
+ 
+        
+
